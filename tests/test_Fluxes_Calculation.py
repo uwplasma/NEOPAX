d4fd4bfa47a45c931ae651fd0347b124705776bb
eduardolneto <eduardo.lascasneto@gmail.com>
2025-05-14 22:30:13 +0100

    Adding _parameters.py file for solver parameters class. Adding test for electric field solver

--- tests/test_Fluxes_Calculation.py
+++ tests/test_Fluxes_Calculation.py
@@ -1,27 +1,31 @@
-#Initialization
-import jax
+#Importing modules
 import jax.numpy as jnp
+import jax
 jax.config.update('jax_platform_name', 'cpu')
-print(jax.devices()) # TFRT_CPU_0
+# before execute any computation / allocation
+print('Using device:', jax.devices())
 from jax import config
 # to use higher precision
 config.update("jax_enable_x64", True)
+import toml
+import interpax
 import h5py as h5
-import matplotlib.pyplot as plt
 import NEOPAX
+import matplotlib.pyplot as plt
 from NEOPAX._constants import elementary_charge
 
-vmec_file='./tests/inputs/wout_QI_nfp2_newNT_opt_hires.nc'
-boozer_file='./tests/inputs/boozermn_wout_QI_nfp2_newNT_opt_hires.nc'
-neoclassical_file='./tests/inputs/Dij_NEOPAX_FULL_S_NEW_Er_Opt.h5'
-neoclassical_option= 1
+#Opening NTSS file for solution with and without momentum correction
+file_Initial=h5.File('./inputs/NTSS_W7X_Initial.h5','r')
+Er_Initial=interpax.Interpolator1D(file_Initial['r'][()],file_Initial['Er'][()],extrap=True)
 
-#Solver parameters 
-parameters=NEOPAX.Solver_Parameters()
+vmec_file='./inputs/wout_W7-X_standard_configuration.nc'
+boozer_file='./inputs/boozmn_wout_W7-X_standard_configuration.nc'
+neoclassical_file='./inputs/Dij_NEOPAX_FULL_S_NEW_W7X.h5'
+neoclassical_option= 1
 
 #Create grid
 n_species=3
-Nx=4
+Nx=64
 n_radial=51
 grid=NEOPAX.Grid.create_standard(n_radial,Nx,n_species)
 
@@ -54,7 +58,7 @@ TD_initial_function= lambda r: T_scale*((ti0-tib)*(1-(r/field.a_b)**2)+tib)
 nD_initial_function = lambda r: n_scale*deuterium_ratio*((ni0-nib)*(1-(r/field.a_b)**10.)+nib)
 TT_initial_function= lambda r: T_scale*((ti0-tib)*(1-(r/field.a_b)**2)+tib)
 nT_initial_function = lambda r: n_scale*tritium_ratio*((ni0-nib)*(1-(r/field.a_b)**10.)+nib)
-Er_initial_function = lambda x: 100.*x*(0.8-x)
+Er_initial_function = lambda x: Er_Initial(x)
 
 #Er0 = SpatialDiscretisation.discretise_fn(r0, r_final, n_radial, Er_initial_function)
 
@@ -65,7 +69,7 @@ TD_initial=TD_initial_function(field.r_grid)
 nD_initial=nD_initial_function(field.r_grid)
 TT_initial=TT_initial_function(field.r_grid)
 nT_initial=nT_initial_function(field.r_grid)
-Er_initial=Er_initial_function(field.rho_grid)
+Er_initial=Er_initial_function(field.r_grid)
 
 
 temperature_initial=jnp.zeros((n_species,n_radial))
@@ -76,12 +80,6 @@ temperature_initial=temperature_initial.at[2,:].set(TT_initial)
 density_initial=density_initial.at[0,:].set(ne_initial)
 density_initial=density_initial.at[1,:].set(nD_initial)
 density_initial=density_initial.at[2,:].set(nT_initial)
-
-fr=0.0
-temperature_initial=temperature_initial.at[:,0].set((4.*temperature_initial.at[:,1].get()-temperature_initial.at[:,2].get()-fr*2*field.dr)/(3.0))   
-temperature_initial=temperature_initial.at[:,-1].set(T_edge)
-density_initial=density_initial.at[:,0].set((4.*density_initial.at[:,1].get()-density_initial.at[:,2].get()-fr*2*field.dr)/(3.0))   
-density_initial=density_initial.at[:,-1].set(n_edge)
 mass=jnp.array([1 / 1836.15267343,2,3])
 charge=jnp.array([-1,1,1])
 
@@ -92,44 +90,26 @@ Global_species=NEOPAX.Species(n_species,n_radial,grid.species_indeces,mass,charg
 #Read monoenergetic database, from MONKES 
 database=NEOPAX.Monoenergetic.read_monkes(field.a_b,neoclassical_file)
 
-turbulent=NEOPAX.Turbulence.from_analytical_model(Global_species,jnp.zeros(3))
 
-#Initial profiles
-ne0=Global_species.density[0]*1.e-20
-nD0=Global_species.density[1]*1.e-20
-nT0=Global_species.density[2]*1.e-20
-Pe0=Global_species.temperature[0]*1.e-3*ne0
-PD0=Global_species.temperature[1]*1.e-3*nD0
-PT0=Global_species.temperature[2]*1.e-3*nT0
-Er0=Global_species.Er
+#Calculating fluxes with momentum correction and without momentum correction
 
-#Define initial coupled solutions to diffrax (can solve tupples of spatial descritizations!!! already tested)
-y0=(Er0,Pe0,PD0,PT0,ne0,nD0,nT0)#,ne_initial,nT_initial,nD_initial,nHe_initial in r=rho*a_b!!!!
-args=Global_species,grid,field,database,turbulent,parameters
-
-
-sol=NEOPAX.solve_transport_equations(y0,args)
-
-Er_final=sol.ys[0][-1,:]
-
-
-Er_final_solution=jnp.array([-1.22315383e-28,  1.34148416e+00,  2.68049898e+00,  4.01457838e+00,  5.34141786e+00,  6.65883724e+00,  7.96491378e+00,  9.25819838e+00,
-  1.05379801e+01,  1.18045717e+01,  1.30596103e+01,  1.43065483e+01,  1.55528346e+01,  1.68249640e+01,  1.81298768e+01,  1.94465836e+01,
-  2.07675719e+01,  2.20864651e+01,  2.33955143e+01,  2.46796001e+01,  2.59154377e+01,  2.71024752e+01,  2.82296743e+01,  2.92845702e+01,
-  3.02548772e+01,  3.11273140e+01,  3.18774798e+01,  3.25045423e+01,  3.29713771e+01,  3.31124574e+01,  3.20962431e+01,  2.57181512e+01,
- -5.76493735e+00, -6.66467296e+00, -7.18522100e+00, -7.75864110e+00, -8.40553406e+00, -9.14665133e+00, -1.00175170e+01, -1.10641749e+01,
- -1.22968727e+01, -1.37198451e+01, -1.53452514e+01, -1.72394724e+01, -1.94734517e+01, -2.21161817e+01, -2.51286207e+01, -2.84218098e+01,
- -3.28134826e+01, -4.04411762e+01, -5.49779014e+01])
-
-
-assert not jnp.all(jnp.abs(Er_final-Er_final_solution)) < 1.e-6, "Electric field solution matches correct value."
+Lij,Gamma,Q,Upar=NEOPAX.get_Neoclassical_Fluxes(Global_species,grid,field,database)
 
 
 
+J_final=elementary_charge*(-Upar[0]+Upar[1]+Upar[2])
+
+J_final_solution=jnp.array([      0.,         45168.07982563,   95688.8335667,   155825.74970629,  228581.28274038,  315657.68784945,  417446.43776736,  533218.09844003,
+  661089.10371784,  797792.20906534,  939562.66801993, 1081403.75730233, 1217938.51171592, 1326921.75818245, 1402303.70751863, 1459721.67655899,
+ 1500096.34683585, 1525191.88607759, 1537429.402792,   1540022.41980644, 1534879.80792856, 1516742.23711296, 1485087.24827067, 1445172.12561103,
+ 1405434.79877314, 1375563.00896985, 1375873.40553034, 1400463.96764785, 1447399.89938824, 1509482.97625722, 1576839.96538312, 1640499.07468469,
+ 1685269.78960362, 1715367.54473189, 1734068.28948583, 1742016.50079477, 1743352.64694388, 1741526.94511271, 1742317.19748327, 1748390.1280721,
+ 1756702.56910076, 1773933.69814733, 1817689.98340421, 1915766.4127553, 2047554.44333294, 2051217.90138083, 1819614.4363332,  1377410.70531666,
+  817668.10002162,  289681.33337484,  -77014.35955482])
 
 
+Final=jnp.abs(J_final-J_final_solution)
 
 
-
-
-
+assert not jnp.all(Final[1:19]) < 1.e-6 ,"Electric field solution matches correct value."
+assert not jnp.all(Final[20:]) < 1.e-6 , "Electric field solution matches correct value."
