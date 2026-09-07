32d28a6363747b87c47a6a0d51cf021dba63d13d
eduardolneto <exouser@image-build-instance-ubuntu-24-1764415186875589804.js2local>
2026-09-07 08:06:27 +0000

    Adding tomls

--- NEOPAX/version.py
+++ NEOPAX/version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.dev819+gebd8511d5.d20260618'
-__version_tuple__ = version_tuple = (0, 1, 'dev819', 'gebd8511d5.d20260618')
+__version__ = version = '0.1.dev1897+gc026b20c3.d20260903'
+__version_tuple__ = version_tuple = (0, 1, 'dev1897', 'gc026b20c3.d20260903')
 
-__commit_id__ = commit_id = 'gebd8511d5'
+__commit_id__ = commit_id = 'gc026b20c3'
